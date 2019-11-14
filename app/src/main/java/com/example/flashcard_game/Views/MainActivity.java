09fe468c845462c3921ac1777a6574a50ffc223e
Phanhung <hungphan.codecomplete@gmail.com>
2019-11-14 15:23:03 +0700

    Define code structure and create models

--- app/src/main/java/com/example/flashcard_game/Views/MainActivity.java
+++ app/src/main/java/com/example/flashcard_game/Views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.flashcard_game;
+package com.example.flashcard_game.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.flashcard_game.R;
 
 public class MainActivity extends AppCompatActivity {
 
