56bd9f06fd1a1763e971503b8c35b12a262fb31f
PhanHung <hungphan.codecomplete@gmail.com>
2020-01-07 11:21:14 +0700

    Update HighScoreFragment

--- app/src/main/java/com/ccvn/flashcard_game/views/HighScoreFragment.java
+++ app/src/main/java/com/ccvn/flashcard_game/views/HighScoreFragment.java
@@ -2,9 +2,13 @@ package com.ccvn.flashcard_game.views;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -13,10 +17,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.Toast;
 
 import com.ccvn.flashcard_game.R;
 import com.ccvn.flashcard_game.databinding.HighscoreFragmentBinding;
@@ -40,10 +46,18 @@ public class HighScoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
          binding = DataBindingUtil.inflate(inflater,R.layout.highscore_fragment, container, false);
-
+            setupToolbar();
         return binding.getRoot();
+    }
+
+    private void setupToolbar(){
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(binding.toolbar);
+
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        activity.getSupportActionBar().setDisplayShowHomeEnabled(true);
+        activity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_black_24dp);
     }
 
     private void runLayoutAnimation(final RecyclerView recyclerView){
@@ -55,6 +69,13 @@ public class HighScoreFragment extends Fragment {
         recyclerView.getAdapter().notifyDataSetChanged();
         recyclerView.scheduleLayoutAnimation();
 
+        binding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
